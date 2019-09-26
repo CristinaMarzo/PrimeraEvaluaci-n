2824bcd18f5e9b6cbcde5d0e2e5430e9db5f1b32
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2019-09-26 09:39:41 +0200

    Add files via upload

---
+++ Saludador_2.py
@@ -0,0 +1,6 @@
+def saludador_2():
+    print "Hola amigo" #Instruccion de salida
+    nombre = raw_input ("Como te llamas?: ") #Instruccion de entrada
+    print "Bienvenid@ a Python" + " " + nombre
+
+saludador_2 ()

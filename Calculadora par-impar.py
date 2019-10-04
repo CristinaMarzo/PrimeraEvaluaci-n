4824d18f428e0417cfcfacf60bf11f078c53d92e
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2019-10-04 09:46:24 +0200

    Add files via upload

---
+++ Calculadora par-impar.py
@@ -0,0 +1,9 @@
+def (calculadora):
+    print "pares e impares"
+    num=int(raw_input('introduce algun numero entero'))
+    if num%2==
+        print 'este numero es par'
+    if num%2!=0
+        print 'este numero es impar'
+
+calculadora()

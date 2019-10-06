b8f468af895f85bc6a92a34aa926ae45bc4efacd
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2019-10-06 22:30:56 +0200

    Add files via upload

---
+++ Calculadora par-impar.py
@@ -0,0 +1,9 @@
+def calculadora():
+    print 'Bienvenido a la calculadora de numeros pares o impares.'
+    n = input('Introduzca su numero aqui: ')
+    if n % 2 ==0:
+        print 'Su numero es par'
+    else:
+        print 'Su numero es impar'
+
+calculadora()
---
+++ Contador primos.py
@@ -0,0 +1,24 @@
+def contador():
+    print 'Bienvenid@ al contador de numeros primos.'
+    print 'Hasta que numero quieres que cuente? Recuerda que tiene que ser positivo.'
+    n = int(input('Ponlo aqui: '))
+    if n<=0:
+        print 'Ha ocurrido un error. Introduzca un numero positivo.'
+        n = int(input('Ponlo aqui: '))
+    else:
+        print 'El numero es correcto.'
+        for i in range (2, n):
+            creciente = 2
+            esprimo = True
+            while esprimo and creciente<i:
+                if i % creciente ==0:
+                    esprimo = False
+                else:
+                    creciente=creciente+1
+            if esprimo:
+                print (i, ' es primo')
+            else:
+                print (i, ' no es primo')
+
+
+contador()

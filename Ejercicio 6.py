d514f85fa408fc433bbd6ad15a5305ef4dfe8e73
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2019-11-07 21:44:38 +0100

    Update Ejercicio 6.py

--- Ejercicio 6.py
+++ Ejercicio 6.py
@@ -1,18 +1,18 @@
 def operaciones():
     print "Bienvenid@ al programa calculadora de operaciones."
     print "Puede sumar, restar, multiplicar o dividir."
-    print "Recuerde que para sumas tendra que introducir 's',"
-    print "'r' para restas, 'm' para multiplicaciones y 'd' para divisiones."
+    print "Recuerde que para sumas tendra que introducir 'S',"
+    print "'R' para restas, 'M' para multiplicaciones y 'D' para divisiones."
     operacion = raw_input("Introduzca aqui la operacion deseada: ")
     n_1 = input ("Introduzca su primer numero aqui: ")
     n_2 = input ("Introduzca su segundo numero aqui: ")
-    if (operacion == "s"):
+    if (operacion == 'S'):
         resultado = n_1 + n_2
-    if (operacion == "r"):
+    if (operacion == 'R'):
         resultado = n_1 - n_2
-    if (operacion == "m"):
+    if (operacion == 'M'):
         resultado = n_1 * n_2
-    if (operacion == "d"):
+    if (operacion == 'D'):
         resultado = n_1 / n_2
     print "Su resultado es: ", resultado
 operaciones()

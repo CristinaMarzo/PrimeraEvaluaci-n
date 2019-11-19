960e1627831c04495db8f67ba9cfc72ea5c9a3c8
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2019-11-19 12:00:45 +0100

    Add files via upload

---
+++ Pajarita.py
@@ -0,0 +1,8 @@
+def pajarita_clase():
+    altura =  input("Cual es la altura deseada?: ")
+    espacios = ' '
+    asteriscos = '*'
+    for i in range (altura/2):
+        
+    
+pajarita_clase()
